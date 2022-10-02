e13d439d9adead40dc9700d892ee86a30fbbbe27
Patrick Plenefisch <simonpatp@gmail.com>
2022-10-02 12:52:42 -0400

    Create higher level gem templates

---
+++ process-wrapper-high/lib/process/wrapper/high.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "high/version"
+
+module Process
+  module Wrapper
+    module High
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ process-wrapper-high/lib/process/wrapper/high/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Process
+  module Wrapper
+    module High
+      VERSION = "0.1.0"
+    end
+  end
+end

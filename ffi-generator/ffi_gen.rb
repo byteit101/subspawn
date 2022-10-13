313ee89964c815ea8d76471048bb47a7420b5b8c
Patrick Plenefisch <simonpatp@gmail.com>
2022-10-13 00:00:50 -0400

    version change due to dev work

--- ffi-generator/ffi_gen.rb
+++ ffi-generator/ffi_gen.rb
@@ -548,6 +548,6 @@ end
 
 #puts builder.to_s
 
-puts builder.export("lfp", ["spawnattr", "spawn_file_actions"], {"spawnattr" => "spawn"}, "LFP", "LFPFile.local_so", "0.4.3") unless live
+puts builder.export("lfp", ["spawnattr", "spawn_file_actions"], {"spawnattr" => "spawn"}, "LFP", "LFPFile.local_so", "0.5.0-dev") unless live
 
 

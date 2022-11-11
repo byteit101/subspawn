d8119cb43f1c2d555599c3e670984e6239c69c3f
Patrick Plenefisch <simonpatp@gmail.com>
2022-11-10 19:05:48 -0500

    rlimit fixes for 0.5.0

--- subspawn-posix/lib/subspawn/posix/ffi_helper.rb
+++ subspawn-posix/lib/subspawn/posix/ffi_helper.rb
@@ -1,14 +1,6 @@
 require 'ffi'
 
 module SubSpawn::POSIX::Internal
-	class Rlimit < FFI::Struct
-		layout(
-		# The current (soft) limit.
-		:rlim_cur,	:rlim_t,
-		#The hard limit.
-		:rlim_max,	:rlim_t
-		)
-	end
 	module SignalFn
 		extend FFI::Library
 		ffi_lib FFI::Library::LIBC

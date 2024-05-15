4b3a5a6edfcc2e78728bee391cb935a42b240c34
Patrick Plenefisch <simonpatp@gmail.com>
2024-05-14 22:14:47 -0400

    "edge" conditions

--- binary-builders/setup.sh
+++ binary-builders/setup.sh
@@ -1,5 +1,5 @@
 
-
+touched=false
 if [ "x${BINARY_SET}" == "x" ]
 then
 	BINARY_SET=ALL
@@ -15,12 +15,16 @@ if [ ${BINARY_SET} == "ALL" ] || [ ${BINARY_SET} == "linux-arm" ]; then
 	docker run --rm dockcross/linux-arm64 > ./dockcross-linux-arm64
 	docker run --rm dockcross/linux-armv7 > ./dockcross-linux-armv7
 	docker run --rm dockcross/linux-armv6 > ./dockcross-linux-armv6
+	touched=true
 fi
 #docker run --rm dockcross/linux-riscv32 > ./dockcross-linux-riscv32
 #docker run --rm dockcross/linux-riscv64 > ./dockcross-linux-riscv64
 if [ ${BINARY_SET} == "ALL" ] || [ ${BINARY_SET} == "linux-intel" ]; then
 	docker run --rm dockcross/linux-x86 > ./dockcross-linux-x86
 	docker run --rm dockcross/linux-x64 > ./dockcross-linux-x64
+	touched=true
 fi
 
-chmod +x ./dockcross-*
+if [ $touched == "true" ] ; then
+	chmod +x ./dockcross-*
+fi
